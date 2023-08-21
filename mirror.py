6e72fde514af671e3c7237b43dbfb6f114335085
frictious <gegbaid@gmail.com>
2023-08-21 16:42:41 +0000

    Mirror Dimension Assignment Submission

---
+++ mirror.py
@@ -0,0 +1,3 @@
+word = input("Enter the word that should be Mirrored: ") # Taking the word that should be mirrored
+mirror = word[::-1] # Slicing the word from the last character
+print(mirror) # Printing the mirrored word
